fef2d890d3cec9b25c6fbe145929841de3c70fc4
Rocky Eastman Jr <eastmanrjr@gmail.com>
2019-12-20 21:23:54 -0800

    Test video player

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,39 @@
 // Imports
 //-----------------------------------------------------------------------------
 import React from 'react'
+import styled from 'styled-components'
 
 //-----------------------------------------------------------------------------
 // Component
 //-----------------------------------------------------------------------------
 const App = () => {
   return (
-    <div className="App">
-      Jake and Malia's website
-    </div>
+    <Container>
+      <StyledVideo
+        controls
+        src="https://jakeandmalia.s3.amazonaws.com/Pexels+Videos+2330708.mp4"/>
+    </Container>
   )
 }
+
+//-----------------------------------------------------------------------------
+// Styled Components
+//-----------------------------------------------------------------------------
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  padding: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledVideo = styled.video`
+  width: 100%;
+  height: auto;
+`
 
 export default App
